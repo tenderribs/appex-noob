19d0aa6e8708cca499e9b76dd1d60bb32db6c7a7
MaJeD <majed@prine.ch>
2018-05-13 20:49:33 +0200

    added base dir constant to all require_once

--- anzeige.php
+++ anzeige.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
 <html>
     <head>
- <?php require_once('metaData.php');   ?>
+ <?php 
+ require_once(__DIR__.'./metaData.php'); 
+     ?>
     </head>
     <body>
       <?php
             //navbar
-          require_once('kopfzeile.php');
+          require_once(__DIR__.'./kopfzeile.php');
           $page = isset($_GET["page"])?$_GET["page"]:'';
             //page content
             echo '<div class="section">';
           if ($page == '') {
-            require_once('pages/zuhause.html');
+            require_once(__DIR__.'./pages/zuhause.html');
           }
            else if ($page == 'funktionen') {
-            require_once('pages/funktionen.html');
+            require_once(__DIR__.'./pages/funktionen.html');
           }
            else if ($page == 'deBlog') {
-            require_once('pages/deBlog.html');
+            require_once(__DIR__.'./pages/deBlog.html');
           }
           echo '</div>';
       ?>      
-      <?php require_once('footer.php');   ?>
+      <?php require_once(__DIR__.'./footer.php');   ?>
     </body>
 </html>
--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
 <html>
     <head>
- <?php require_once('metaData.php');   ?>
+ <?php 
+ require_once(__DIR__.'./metaData.php');   
+ ?>
     </head>
     <body>
       <?php
             //navbar
-          require_once('header.php');
+          require_once(__DIR__.'./header.php');
           $page = isset($_GET["page"])?$_GET["page"]:'';
             //page content
         echo '<div class="section">';
           if ($page == '') {
-            require_once('pages/home.html');
+            require_once(__DIR__.'./pages/home.html');
           }
            else if ($page == 'features') {
-            require_once('pages/features.html');
+            require_once(__DIR__.'./pages/features.html');
           }
            else if ($page == 'blog') {
-            require_once('pages/blog.html');
+            require_once(__DIR__.'./pages/blog.html');
           }
           echo '</div>';
       ?>
-      <?php require_once('footer.php');   ?>
+      <?php require_once(__DIR__.'./footer.php');   ?>
     </body>
 </html>
