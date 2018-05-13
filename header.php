c231ae59b3b5db09436170dc4646122fe4d0f121
tenderribs <dc3dude@gmail.com>
2018-05-13 17:55:34 +0200

    deleted noob code

--- header.php
+++ header.php
@@ -43,27 +43,3 @@
     </div>
   </div>
 </nav>
-
-<!-- <nav class="navbar is-fixed-top" style="background-color: #448afc" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-            <a class="navbar-item has-text-black-bis" style="background-color: rgb(238, 238, 238)" class="appex-navbar" href="../index.php">
-
-            <img src="images\logo.gif" alt="Applied Experience" width="75" height="32">
-
-            <h3><strong>Applied</strong> Experience</h3>
-            </a>
-            <a class="navbar-item has-text-black-ter" href="index.php?page=features">
-                Learn More / About Us
-            </a>
-            <a class="navbar-item has-text-black-ter" href="index.php?page=blog">
-                Contact & Blog
-            </a>
-        </div>
-
-        <div class="navbar-end">
-            <div class="navbar-item">
-            <a href="index.php"><button style="background-color: #3dce5f"  class="button">EN</button></a>
-            <a href="anzeige.php"><button class="button">DE</button></a>
-            </div>
-        </div>                   
-</nav> -->
