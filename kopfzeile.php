b8fb04dfca50643154ecaf3a997180285430f109
MaJeD <majed@prine.ch>
2018-05-13 18:41:46 +0200

    removed unused codes

--- kopfzeile.php
+++ kopfzeile.php
@@ -43,24 +43,3 @@
     </div>
   </div>
 </nav>
-            <!-- <nav class="navbar is-fixed-top" style="background-color: #448afc" role="navigation" aria-label="main navigation">
-                    <div class="navbar-brand">
-                      <a class="navbar-item has-text-black-bis" style="background-color: #ededed" class="appex-navbar" href="../anzeige.php">
-                        <img src="images\logo.gif" alt="Applied Experience" width="75" height="32">
-                        <h3><strong>Applied Experience</strong></h3>
-                      </a>
-                        <a class="navbar-item has-text-black-ter" href="anzeige.php?page=funktionen">
-                          Mehr erfahren / Über uns
-                        </a>
-                        <a class="navbar-item has-text-black-ter" href="anzeige.php?page=deBlog">
-                          Kontakt und Blog
-                        </a>
-                    </div>
-
-                    <div class="navbar-end">
-                      <div class="navbar-item">
-                      <a href="index.php"><button class="button">EN</button></a>
-                      <a href="anzeige.php"><button class="button" style="background-color: #3dce5f">DE</button></a>                   
-                      </div>
-                    </div>                   
-            </nav> -->
